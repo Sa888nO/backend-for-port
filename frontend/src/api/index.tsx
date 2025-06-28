d498db9a9c1ccf1400d921fd4bf348ac88dfcd52
AlexEgorushkin <shura.reno.97@bk.ru>
2025-06-29 01:53:27 +0300

    fx

--- frontend/src/api/index.tsx
+++ frontend/src/api/index.tsx
@@ -11,8 +11,15 @@ export const Api = axios.create({
 export const AuthApi = axios.create({
     baseURL: 'http://5.35.98.185:4444/api',
     // baseURL: 'http://localhost:5000/api',
-    headers: { Authorization: `Bearer ${cookies.get('token')}` },
+    // headers: { Authorization: `Bearer ${cookies.get('token')}` },
 });
+AuthApi.interceptors.request.use(
+    (config) => {
+        config.headers.Authorization = `Bearer ${cookies.get('token')}`;
+        return config;
+    },
+    (error) => Promise.reject(error),
+);
 
 export const Static = axios.create({
     baseURL: 'http://5.35.98.185:4444',
