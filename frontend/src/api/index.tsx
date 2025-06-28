494268893b52fe530ea22b7f1a154efa6d60e2cd
AlexEgorushkin <shura.reno.97@bk.ru>
2025-06-29 00:17:06 +0300

    fx docker

--- frontend/src/api/index.tsx
+++ frontend/src/api/index.tsx
@@ -4,17 +4,17 @@ import Cookies from 'universal-cookie';
 const cookies = new Cookies();
 
 export const Api = axios.create({
-    // baseURL: 'http://5.35.98.185:4444/api',
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'http://5.35.98.185:4444/api',
+    // baseURL: 'http://localhost:5000/api',
 });
 
 export const AuthApi = axios.create({
-    // baseURL: 'http://5.35.98.185:4444/api',
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'http://5.35.98.185:4444/api',
+    // baseURL: 'http://localhost:5000/api',
     headers: { Authorization: `Bearer ${cookies.get('token')}` },
 });
 
 export const Static = axios.create({
-    // baseURL: 'http://5.35.98.185:4444',
-    baseURL: 'http://localhost:5000',
+    baseURL: 'http://5.35.98.185:4444',
+    // baseURL: 'http://localhost:5000',
 });
